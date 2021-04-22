096e98a65e153840578a3b486dd186a501697052
LucasAMiranda <lucasa.miranda@hotmail.com>
2021-04-21 23:46:53 -0300

    Atualizações do projeto na parte client , falta corrigir pequenos bugs!

--- client.py
+++ client.py
@@ -1,36 +1,47 @@
 import socket
-import sys
 import json
 
 
-class Client:
-    def __init__(self, server_address, config_file):
-        # Create a TCP/IP socket
-        sockClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class Cliente:
+    def __init__(self):
+        with open('config.json') as config_file:
+            self.config = json.loads(config_file)
 
-        self.config = json.load(config_file)
+        # Create a TCP/IP socket
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         # Connect the socket to the port where the server is listening
-        server_address = (self.config['ip'], self.config['port'])
-        print(sys.stdout, 'connecting to %s port %s' % server_address)
-        sockClient.connect(server_address)
-        sockClient.accept()
+        self.client.connect(client.run())
+        self.client.accept()
 
-        try:
+    # Executable in port and ip , listining all connections in ports and conversations
+    def run(self):
 
-            # sending  data
-            message = 'the connection has been successfully established'
-            print(message)
-            sockClient.sendall(message)
+        msg = bytes(input("Digite algo: "), 'utf-8')
 
-           # Fetching connection response
-            received = 0
-            expected = len(message)
+        self.client.send(msg)
+        answer = self.config.recv(1024)
+        print("Recebi mensagem ", answer)
 
-            while received < expected:
-                data = sockClient.recv(1024)
-                received += len(data)
-                print(sys.stdout, 'received data', data)
-        finally:
-            print(sys.stdout, 'closing socket')
-            sockClient.close()
+        self.client.listen(
+            self.config[{'ip': ''}], self.config[{'port': ''}], self.config[{'max-connections': ''}])
+
+        for c in self.config[{'port'}]:
+            print(c)
+
+    # Stop connections
+    def stop(self):
+        self.client.close()
+
+
+if __name__ == '__main__':
+    try:
+        client = Cliente()
+
+        while not client.client._closed:
+            pass
+            ...
+
+    except Exception as error:
+        print(error)
+        client.stop()
