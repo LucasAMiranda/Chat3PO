595f373b8ea5d97c2d77f7a98cd31707c51060db
LSchool-Info-Tech <35241256+LucasAMiranda@users.noreply.github.com>
2021-04-22 20:59:22 -0300

    Update client.py

--- client.py
+++ client.py
@@ -4,44 +4,45 @@ import json
 
 class Cliente:
     def __init__(self):
-        with open('config.json') as config_file:
-            self.config = json.loads(config_file)
+        with open('config.json', 'r') as config_file:
+            try:
+                self.config = json.load(config_file)
+            except json.decoder.JSONDecodeError:
+                self.client = dict()
+
+        with open("config.json", 'w') as fp:
+            json.dump(self.client, fp)
 
         # Create a TCP/IP socket
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    def run(self):
+        address = (self.client['ip'], self.client['port'])
+        self.client.bind(address)
+
         # Connect the socket to the port where the server is listening
-        self.client.connect(client.run())
+        self.client.connect(address)
+
+        self.client.listen(self.client['max-connections'])
+
         self.client.accept()
 
-    # Executable in port and ip , listining all connections in ports and conversations
-    def run(self):
-
-        msg = bytes(input("Digite algo: "), 'utf-8')
-
-        self.client.send(msg)
-        answer = self.config.recv(1024)
-        print("Recebi mensagem ", answer)
-
-        self.client.listen(
-            self.config[{'ip': ''}], self.config[{'port': ''}], self.config[{'max-connections': ''}])
-
-        for c in self.config[{'port'}]:
-            print(c)
-
     # Stop connections
+
     def stop(self):
+
         self.client.close()
 
 
-if __name__ == '__main__':
-    try:
-        client = Cliente()
+    # Executable in port and ip , listining all connections in ports and conversations
+try:
+    client = Cliente()
+    client.run()
 
-        while not client.client._closed:
-            pass
-            ...
-
-    except Exception as error:
-        print(error)
-        client.stop()
+    while not client.client._closed:
+        pass
+        ...
+except Exception as error:
+    print(error)
+    client.stop()
+    raise
